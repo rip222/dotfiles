9c44e6ec03c1a45d3d6469f0f26daaa199981280
Ruslan K <ruslan@me>
2024-10-25 16:14:53 +0200

    nvim: readd comment

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -63,6 +63,7 @@ local plugins = {
   'navarasu/onedark.nvim', -- Theme inspired by Atom
   'nvim-lualine/lualine.nvim', -- Fancier statusline
   'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
+  'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   'windwp/nvim-autopairs', -- Auto closing brackets etc
